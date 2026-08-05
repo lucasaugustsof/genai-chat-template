1f265b466eac47228c37aceb48aa7f43e957b008
Lucas Augusto <lucasaugustscode@gmail.com>
2026-08-05 20:39:33 -0300

    chore: project setup

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="grid min-h-screen place-items-center bg-neutral-950">
+      <h1 className="bg-linear-to-r from-neutral-400 to-transparent bg-clip-text text-8xl leading-snug font-medium text-transparent">
+        Create. Develop. Ship.
+      </h1>
+    </div>
+  );
+}
